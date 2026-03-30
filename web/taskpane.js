79790d330b3098cfa708acda179226dd4a172489
maxkndz <maximilian.kandzia@gmail.com>
2026-03-30 10:44:18 +0200

    Add files via upload

--- web/taskpane.js
+++ web/taskpane.js
@@ -14,7 +14,7 @@ const rechnungInhaltlichMap = {
   ri1: "die Vollständigkeit und Richtigkeit des Prüfblattes zur Rechnungsprüfung",
   ri2: "die Einhaltung vertraglicher Rahmenbedingungen",
   ri3: "die Übereinstimmung der abgerechneten Einheitspreise mit dem Leistungsverzeichnis / Auftrag",
-  ri4: "die Abweichungen der abgerechneten Mengen im Vergleich zum vereinbarten Leistungsumfang",
+  ri4: "die Abweichungen in Mengen und Massen (max. + 10%), bezogen auf den vereinbarten Leistungsumfang",
   ri5: "die rechnerische Richtigkeit sowie die korrekte Übertragung der Werte",
   ri6: "die Nachvollziehbarkeit der Nachweise / Anlagen"
 };
@@ -210,7 +210,9 @@ function replaceSrBlockInHtml(bodyHtml, blockHtml) {
     return bodyHtml.replace(markerRegex, blockHtml);
   }
 
-  return blockHtml + bodyHtml;
+  // Fallback für altes Outlook:
+  // Wenn Marker nicht gefunden werden, nichts löschen, sondern neu oben einfügen.
+  return blockHtml + "<br>" + bodyHtml;
 }
 
 function trimTrailingEmptyHtml(html) {
